e393ef778520b4df06bdc277346d8378b59e95cc
Andres Zapata <afzar27@gmail.com>
2024-08-07 11:14:57 -0500

    feat: init project spring webflux native

---
+++ spring-webflux-native/src/main/java/co/example/springwebfluxnative/SpringWebfluxNativeApplication.java
@@ -0,0 +1,13 @@
+package co.example.springwebfluxnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxNativeApplication.class, args);
+    }
+
+}
